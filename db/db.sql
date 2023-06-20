dc6251ece1a98a452226d582b08400310936a8b1
qqqq4u <danila16102003@rambler.ru>
2023-06-20 22:30:27 +0300

    try with indexes

--- db/db.sql
+++ db/db.sql
@@ -1,11 +1,11 @@
-CREATE EXTENSION IF NOT EXISTS CITEXT;
+ CREATE EXTENSION IF NOT EXISTS CITEXT;
 
 CREATE UNLOGGED TABLE "user"
 (
     Nickname CITEXT COLLATE "C" PRIMARY KEY,
     FullName TEXT NOT NULL,
     About    TEXT NOT NULL DEFAULT '',
-    Email    CITEXT COLLATE "C" UNIQUE
+    Email    CITEXT COLLATE "C"
 );
 
 CREATE UNLOGGED TABLE forum
@@ -57,11 +57,11 @@ CREATE UNLOGGED TABLE vote
 
 CREATE UNLOGGED TABLE user_forum
 (
-    Nickname  CITEXT  NOT NULL,
-    FullName  TEXT    NOT NULL,
-    About     TEXT,
-    Email     CITEXT,
-    Slug      CITEXT  NOT NULL,
+    Nickname CITEXT NOT NULL,
+    FullName TEXT   NOT NULL,
+    About    TEXT,
+    Email    CITEXT,
+    Slug     CITEXT NOT NULL,
     FOREIGN KEY (Nickname) REFERENCES "user" (Nickname),
     FOREIGN KEY (Slug) REFERENCES "forum" (Slug),
     UNIQUE (Nickname, Slug)
@@ -72,11 +72,12 @@ $update_forum_posts$
 DECLARE
     t_fullname CITEXT;
     t_about    CITEXT;
-    t_email CITEXT;
+    t_email    CITEXT;
 BEGIN
     SELECT fullname, about, email FROM "user" WHERE nickname = NEW.author INTO t_fullname, t_about, t_email;
     INSERT INTO user_forum (nickname, fullname, about, email, Slug)
-    VALUES (New.Author, t_fullname, t_about, t_email, NEW.forum) on conflict do nothing;
+    VALUES (New.Author, t_fullname, t_about, t_email, NEW.forum)
+    on conflict do nothing;
     return NEW;
 end
 $update_forum_posts$ LANGUAGE plpgsql;
@@ -91,15 +92,18 @@ EXECUTE PROCEDURE updatePostUserForum();
 CREATE OR REPLACE FUNCTION updateThreadUserForum() RETURNS TRIGGER AS
 $update_forum_threads$
 DECLARE
-    a_nick CITEXT;
+    a_nick     CITEXT;
     t_fullname CITEXT;
     t_about    CITEXT;
-    t_email CITEXT;
+    t_email    CITEXT;
 BEGIN
     SELECT Nickname, fullname, about, email
-    FROM "user" WHERE Nickname = new.Author INTO a_nick, t_fullname, t_about, t_email;
+    FROM "user"
+    WHERE Nickname = new.Author
+    INTO a_nick, t_fullname, t_about, t_email;
     INSERT INTO "user_forum" (nickname, fullname, about, email, slug)
-    VALUES (a_nick, t_fullname, t_about, t_email, NEW.forum) on conflict do nothing;
+    VALUES (a_nick, t_fullname, t_about, t_email, NEW.forum)
+    on conflict do nothing;
     return NEW;
 end
 $update_forum_threads$ LANGUAGE plpgsql;
@@ -113,7 +117,7 @@ EXECUTE PROCEDURE updateThreadUserForum();
 CREATE OR REPLACE FUNCTION ThreadsCountInc() RETURNS TRIGGER AS
 $update_forums$
 BEGIN
-    UPDATE forum SET Threads=(Threads+1) WHERE slug=NEW.forum;
+    UPDATE forum SET Threads=(Threads + 1) WHERE slug = NEW.forum;
     return NEW;
 end
 $update_forums$ LANGUAGE plpgsql;
@@ -127,7 +131,7 @@ EXECUTE PROCEDURE ThreadsCountInc();
 CREATE OR REPLACE FUNCTION insertVotes() RETURNS TRIGGER AS
 $update_vote$
 BEGIN
-    UPDATE thread SET votes=(votes+NEW.voice) WHERE id=NEW.thread;
+    UPDATE thread SET votes=(votes + NEW.voice) WHERE id = NEW.thread;
     return NEW;
 end
 $update_vote$ LANGUAGE plpgsql;
@@ -142,7 +146,7 @@ CREATE OR REPLACE FUNCTION updateVotes() RETURNS TRIGGER AS
 $update_votes$
 BEGIN
     IF OLD.Voice <> NEW.Voice THEN
-        UPDATE thread SET votes=(votes+NEW.Voice*2) WHERE id=NEW.Thread;
+        UPDATE thread SET votes=(votes + NEW.Voice * 2) WHERE id = NEW.Thread;
     END IF;
     return NEW;
 end
@@ -157,7 +161,7 @@ EXECUTE PROCEDURE updateVotes();
 CREATE OR REPLACE FUNCTION updatePath() RETURNS TRIGGER AS
 $update_path$
 DECLARE
-    parent_path  INTEGER[];
+    parent_path   INTEGER[];
     parent_thread int;
 BEGIN
     IF (NEW.parent = 0) THEN
@@ -181,3 +185,34 @@ CREATE TRIGGER u_p_trigger
     ON post
     FOR EACH ROW
 EXECUTE PROCEDURE updatePath();
+
+CREATE INDEX IF NOT EXISTS thread_slug_idx ON "thread" (slug);
+CREATE INDEX IF NOT EXISTS thread_id_idx ON "thread" (id);
+
+CREATE INDEX IF NOT EXISTS post_thread_idx ON "post" (thread);
+CREATE INDEX IF NOT EXISTS post_thread_id_idx ON "post" (thread, id);
+CREATE INDEX IF NOT EXISTS post_id_path1_idx ON "post" (id, (path[1]));
+CREATE INDEX IF NOT EXISTS post_path1_idx ON "post" ((path[1]));
+CREATE INDEX IF NOT EXISTS post_thread_path_idx ON "post" (thread, path, id);
+CREATE INDEX IF NOT EXISTS post_thread_path1_parent_idx ON "post" (thread, id, (path[1]), parent);
+
+CREATE INDEX IF NOT EXISTS user_nickname_hash_idx ON "user" USING hash (nickname);
+CREATE INDEX IF NOT EXISTS user_email_hash_idx ON "user" USING hash (email);
+CREATE INDEX IF NOT EXISTS user_full_idx ON "user" (email, nickname);
+
+CREATE INDEX IF NOT EXISTS forum_slug_hash_idx ON "forum" USING hash (slug);
+
+CREATE INDEX IF NOT EXISTS thread_date_idx ON thread (created);
+CREATE INDEX IF NOT EXISTS thread_forum_date_idx ON thread (forum, created);
+CREATE INDEX IF NOT EXISTS thread_forum_hash_idx ON thread USING hash (forum);
+CREATE INDEX IF NOT EXISTS thr_slug_hash_idx ON thread USING hash (slug);
+
+CREATE INDEX IF NOT EXISTS vote_full_idx ON vote (author, voice, thread);
+
+CREATE INDEX IF NOT EXISTS fu_forum ON user_forum USING hash(Slug);
+
+CREATE UNIQUE INDEX IF NOT EXISTS forum_users_unique ON user_forum (slug, nickname);
+CREATE UNIQUE INDEX IF NOT EXISTS vote_unique ON vote (author, thread);
+
+VACUUM;
+VACUUM ANALYSE;
